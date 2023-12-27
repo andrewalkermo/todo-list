510b35be918b51a15b94520a40b986ae326b9ef5
Andre Walker <andre.walkermo@gmail.com>
2023-12-27 12:35:42 -0300

    refactor: simplifica inserção de itens

--- index.js
+++ index.js
@@ -1,74 +1,82 @@
+window.addEventListener("load", function () {
+  loadTasks();
+});
+
 function getNewTask() {
-  let newTask = document.querySelector('.todo:last-child').cloneNode(true);
-  newTask.querySelector('input[type="text"]').value = '';
-  newTask.querySelector('input[type="text"]').removeAttribute('readonly');
-  newTask.querySelector('input[type="checkbox"]').removeAttribute('checked');
-  newTask.querySelector('input[type="checkbox"]').removeAttribute('disabled');
+  const newTask = document.createElement("div");
+  newTask.classList.add("todo");
+
+  const deleteButton = document.createElement("span");
+  deleteButton.classList.add("delete");
+  deleteButton.innerHTML = "X";
+  deleteButton.addEventListener("click", function () {
+    this.parentNode.remove();
+    saveTasks();
+  });
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("name", "task-completed[]");
+  checkbox.addEventListener("change", () => saveTasks());
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "task-description[]");
+  input.setAttribute("placeholder", "Nova tarefa");
+  input.addEventListener("keyup", function (e) {
+    if (e.code === "Enter") {
+      const newTask = getNewTask();
+      this.parentNode.parentNode.insertBefore(
+        newTask,
+        this.parentNode.nextSibling
+      );
+      newTask.querySelector('input[type="text"]').focus();
+    }
+    saveTasks();
+  });
+
+  newTask.appendChild(deleteButton);
+  newTask.appendChild(checkbox);
+  newTask.appendChild(input);
+
   return newTask;
 }
 
-window.addEventListener('load', function () {
-  listenNewTasks();
-  listenTaskChanges();
-  listenDeleteTasks();
-  loadTasks();
-})
-
-function listenNewTasks() {
-  document.querySelector('.todo:last-child').addEventListener('click', function() {
-    let newTask = getNewTask();
-    this.parentNode.insertBefore(newTask, this);
-    document.querySelector('.todo:nth-last-child(2) input[type="text"]').focus();
-  });
-  document.querySelector('.todo').addEventListener('keyup', function(e) {
-    if (e.code === "Enter") {
-      let newTask = getNewTask();
-      this.parentNode.insertBefore(newTask, this.nextSibling);
-      newTask.focus();
-    }
-  });
-}
-
-function listenTaskChanges() {
-  document.addEventListener('change', function(event) {
-    if (event.target.tagName === 'INPUT') {
-      saveTasks();
-    }
-  });
-}
-
-function listenDeleteTasks() {
-  document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete') && !event.target.parentNode.querySelector('input[type="checkbox"]').disabled) {
-      event.target.parentNode.remove();
-      saveTasks();
-    }
-  });
-}
-
 function saveTasks() {
-  let tasks = document.querySelectorAll('.todo');
-  let tasksArray = [];
-  tasks.forEach(function(task) {
-    if (task.querySelector('input[type="text"]').value) {
-      tasksArray.push({
-        task: task.querySelector('input[type="text"]').value,
-        completed: task.querySelector('input[type="checkbox"]').checked
-      });
-    }
+  const tasks = Array.from(document.querySelectorAll(".todo")).map(function (
+    task
+  ) {
+    return {
+      task: task.querySelector('input[type="text"]').value,
+      completed: task.querySelector('input[type="checkbox"]').checked,
+    };
   });
-  localStorage.setItem('tasks', JSON.stringify(tasksArray));
+
+  if (tasks.length === 0) {
+    insertInitialTask();
+    return;
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function loadTasks() {
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  if (tasks) {
-    tasks.forEach(function(task) {
-      let newTask = getNewTask();
-      newTask.querySelector('input[type="text"]').value = task.task;
-      newTask.querySelector('input[type="checkbox"]').checked = task.completed;
-      
-      document.querySelector('.todo:last-child').parentNode.insertBefore(newTask, document.querySelector('.todo:last-child'));
-    });
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks === null || tasks.length === 0 || typeof tasks === "undefined") {
+    insertInitialTask();
+    return;
   }
+
+  tasks.forEach(function (task) {
+    const newTask = getNewTask();
+    newTask.querySelector('input[type="text"]').value = task.task;
+    newTask.querySelector('input[type="checkbox"]').checked = task.completed;
+
+    document.querySelector(".todo-list").appendChild(newTask);
+  });
+}
+
+function insertInitialTask() {
+  const newTask = getNewTask();
+  document.querySelector(".todo-list").appendChild(newTask);
 }
